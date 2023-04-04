2fa81959f614d8e6d6c36bc9d70799b09ecbff56
Alien Coder <paidcodecamp@gmail.com>
2023-04-04 23:25:18 +0530

    code cleanup

--- server/index.js
+++ server/index.js
@@ -1,4 +1,4 @@
-import express from "express"; 
+import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import bodyParser from "body-parser";
@@ -8,19 +8,19 @@ import Routes from "./routes/route.js";
 
 dotenv.config();
 
-
 const app = express();
-app.use("/",Routes);
+app.use("/", Routes);
 app.use(cors());
 app.use(bodyParser.json({ extended: true }));
-app.use(bodyParser.urlencoded({ extended:true }));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const PORT = 8080;
 
+const username = process.env.DB_USERNAME;
+const password = process.env.DB_PASSWORD;
 
-const username=process.env.DB_USERNAME;
-const password=process.env.DB_PASSWORD;
+connection(username, password);
 
-connection(username,password);
-
-app.listen(PORT, () => console.log('Server is running sucessfully on Port ${PORT}'));
+app.listen(PORT, () =>
+  console.log(`Server is running sucessfully on Port ${PORT}`)
+);
